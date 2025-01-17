12e943234911b22eb9431ea54f8a9d39752c6c0e
JAEHEUNG-LEE <jheung998@gmail.com>
2025-01-17 17:45:25 +0900

    11

--- src/pages/board/BoardAddPage.js
+++ src/pages/board/BoardAddPage.js
@@ -5,8 +5,6 @@ const BoardAddPage = () => {
     <div className="p-4 w-full bg-white">
       <div className="text-3xl font-extrabold ">공지사항 작성페이지</div>
 
-    <div >
-
       <BoardAddComponent />
     </div>
   );
