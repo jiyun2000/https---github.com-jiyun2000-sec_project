0ab60ac99f9b7fb2d4bad33b09c9f7188cb5a609
jiyun2000 <hurgiyun2000@daum.net>
2025-01-20 10:18:32 +0900

    01.20

--- src/components/employees/CommuteModifyComponent.js
+++ src/components/employees/CommuteModifyComponent.js
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react"
 import useCustomMove from "../../hooks/useCustomMove";
 import { getOneCommute, putOne } from "../../api/commuteApi";
+import BoardTitleComponent from '../board/BoardTitleComponent';
+import { Link } from 'react-router-dom';
+import mail from '../../assets/icon/mail.png';
+import chat from '../../assets/icon/chat.png';
 
 const initState = {
     commNo : 0 ,
@@ -36,7 +40,24 @@ const CommuteModifyComponent = ({commNo}) => {
 
     return (
         <div>
-            
+            <div className="flex justify-between items-center w-full bg-white shadow-lg rounded-md mb-8 px-6 py-4">
+            <div className="flex items-center space-x-8">
+                <div className="text-2xl font-semibold text-blue-800 select-none">
+                    [공지사항]
+                </div>
+                <div className="w-64 text-2xl font-semibold cursor-pointer">
+                    <BoardTitleComponent />
+                </div>
+            </div>
+            <div className="flex space-x-4">
+                <Link to="/mail" className="w-12 cursor-pointer">
+                    <img src={mail} alt="Mail" className="w-full" />
+                </Link>
+                <Link to={`/chat/empList/${empNo}?page=1`} className="w-12 cursor-pointer">
+                    <img src={chat} alt="Chat" className="w-full" />
+                </Link>
+            </div>
+        </div>
 
 
         <div className="border-2 border-sky-200 mt-10 m-2 p-4">
